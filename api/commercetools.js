2d04215150b5c9381a8889f308cae52e10e3e515
Gerardo Carmona <gcj@thinkcares.com>
2024-09-08 10:11:51 -0600

    Add CT sdk

---
+++ api/commercetools.js
@@ -0,0 +1,20 @@
+import { SDK, Integration, SDKResponse, ServerOptions } from "@commercetools/frontend-sdk";
+
+
+function getLifetimeOrders(params) {
+    const [ orders, setOrders ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
+    const [ error, setError ] = useState(null);
+
+
+    return null;
+};
+
+function getServiceByReference(params) {
+    const [ service, setService ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
+    const [ error, setError ] = useState(null);
+
+
+    return null;
+};
