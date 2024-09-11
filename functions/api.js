6b57517144d88afd659cf71c177947880249ee1d
Gerardo Carmona <gcj@thinkcares.com>
2024-09-11 14:27:40 -0600

    Activities to implement

--- functions/api.js
+++ functions/api.js
@@ -14,16 +14,32 @@ router.get("/", function(_, res) {
   res.sendStatus(200)
 })
 
+/// authenticacion por AUTH 2.0
+
 router.get("/lifetimes", function(req, res){
     console.log(req.params.client_id)
+    /// Traer ordenes de tipo Combo
+    /// Verificar que ordenes caen en los periodos de notificación:
+    /// 1. 3 meses - antiguedad de la orden sea de 12M
+    /// 2. 1 mese - antiguedad de la orden sea de 13M
+    /// 3. 15 días - antiguedad de la orden sea de 13M y 15d
+    /// 4. 7 días - antiguedad de la orden sea de 13M y 21d
+    /// 5. 1 día antiguedad de la orden sea de 13M y 29d
+    /// Obtener datos del cliente
+    /// armas la estructura de coleccion (data.items)
+
     res.json ({
         statusCode: 200,
         body: data.items,
       });
 });
 
-router.get("/pdv-services/:qr", async function(req, res){
-    const qr = req.params.qr
+router.get("/pdv-services", async function(req, res){
+    const qr = req.headers.qr
+    /// Obtener el QR de una variable en el header
+    /// Obtener orden de CT con query de QR
+    /// Generar la estructura de data.pdvService
+
     console.log(qr)
     const order = await apiRoot.orders().search().post({
       body: {
