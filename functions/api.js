97250bc844bf1c1e6df3ab059a4d1c1c46b89db6
Josue Mendoza <ajpm@thinkcares.com>
2024-09-23 11:21:03 -0600

    TOken

--- functions/api.js
+++ functions/api.js
@@ -4,6 +4,7 @@ import serverless from "serverless-http";
 //import data from './mock_values.json' assert { type: 'json'}
 import { apiRoot } from "../commercetools/client.js";
 import { checkDate } from "./validateDate/validate.js";
+import { generateToken, validateToken } from "../jsonToken/token.js";
 
 const orderstoNotify = {}
 const app = express();
@@ -21,7 +22,6 @@ const addObject = async (index, order, days) => {
     orderstoNotify[index] = []
   }
   try{ 
-    console.log(order.customerId)
     const customer = await apiRoot.customers().withId({ID: order.customerId}).get().execute()
     if(!customer.statusCode || customer.statusCode >= 300) return
 
@@ -45,14 +45,13 @@ const addObject = async (index, order, days) => {
       expirationDays: days
     });
   } catch(err){
-    console.log(err.message)
     return 
   }
 }
 
 /// authenticacion por AUTH 2.0
 
-router.get("/lifetimes", async function(req, res){
+router.get("/lifetimes", validateToken, async function(req, res){
     console.log(req.params.client_id)
     /// Traer ordenes de tipo Combo
     /// Verificar que ordenes caen en los periodos de notificación:
@@ -123,11 +122,134 @@ router.get("/pdv-services", async function(req, res){
     }).execute()
     
     if(order.body.hits.length <= 0) return res.sendStatus(404)
-      res.json ({
-          statusCode: 200,
-          body: order.body.hits[0].id,
-        });
+    
+    const searchOrder = await apiRoot.orders().withId({ID: order.body.hits[0].id}).get().execute()
+    
+    const responseObject = {
+      "pdvService": {
+        "storeServiceOrder": "999-999999",
+        "PurchaseOrder": searchOrder.body.orderNumber,
+        "waybill": "1234567890123456789012",
+        "idcaStoreClient": 1234567890,
+        "idcaServiceWarranty": "123",
+        "idcaServiceModality": "123",
+        "isPudo": "0",
+        "isPackage": "0",
+        "itemLength": "50",
+        "itemHeight": "50",
+        "itemWidth": "50",
+        "itemVolumen": "50",
+        "isItemDimensionsExceeded": "0",
+        "itemWeight": "20",
+        "isItemWeightExceeded": "0",
+        "statusServiceOrder": "Registrado",
+        "QRCode": "1233412444563452342134214323414", // Vacio
+        "QRCodeMD5": "1233412444563452342134214323414",
+        "consultaId": "99999999", /// Revisarlo con Memo
+        "createdDate": "2021-02-24",
+        "availabledDate": "2021-03-01/2021-03-06",
+        "sender": {
+            "eMailClient": "guillermo.valerio@estafeta.com",
+            "isPudo": "0",
+            "EquivalentCode": "123",
+            "TyoeLocationName": "Nombre del Pudo",
+            "SpaceOwnerName": "Tipo de pudo",
+            "isSender": "0",
+            "CompleteName": "JUAN FRANCISCO VALERIO GONZÁLEZ",
+            "zipCode": "99999",
+            "roadTypeCode": "9999",
+            "roadTypeName": "Avenida",
+            "street": "Margarita Maza de Juárez",
+            "externalNum": "Entrada Rada Andador 3 Edificio 100",
+            "indoreInformation": "departamento 17-B",
+            "settlementTypeCode": "999",
+            "settlementTypeName": "Unidad Habitacional",
+            "settlementName": "La Patera Vallejo",
+            "twnshipCode": "999",
+            "twnshipName": "Gustavo A Madero",
+            "stateCode": "999",
+            "stateName": "CDMX",
+            "countryCode": "999",
+            "countryCodeAlfa3": "MEX",
+            "countryName": "México",
+            "betweenRoadName1": "av de los cien metros",
+            "betweenRoadName2": "y calzada vallejo",
+            "AddressReference": "Esta entre el Edificio 97 y 102",
+            "CountryCodePhone": "999",
+            "LandlinePhone": "5555555555",
+            "CellPhone": "5555555555",
+            "ContacteMail": "micorreopersonal@dominio.com",
+            "Latitude": 99999.99,
+            "Longitude": 99999.99,
+            "IsActive": "True",
+            "recipient": {
+                "eMailClient": "guillermo.valerio@estafeta.com",
+                "isPudo": "0",
+                "EquivalentCode": "123",
+                "TyoeLocationName": "Nombre del Pudo",
+                "SpaceOwnerName": "Tipo de pudo",
+                "isSender": "0",
+                "CompleteName": "JUAN FRANCISCO VALERIO GONZÁLEZ",
+                "zipCode": "99999",
+                "roadTypeCode": "9999",
+                "roadTypeName": "Avenida",
+                "street": "Margarita Maza de Juárez",
+                "externalNum": "Entrada Rada Andador 3 Edificio 100",
+                "indoreInformation": "departamento 17-B",
+                "settlementTypeCode": "999",
+                "settlementTypeName": "Unidad Habitacional",
+                "settlementName": "La Patera Vallejo",
+                "twnshipCode": "999",
+                "twnshipName": "Gustavo A Madero",
+                "stateCode": "999",
+                "stateName": "CDMX",
+                "countryCode": "999",
+                "countryCodeAlfa3": "MEX",
+                "countryName": "México",
+                "betweenRoadName1": "av de los cien metros",
+                "betweenRoadName2": "y calzada vallejo",
+                "AddressReference": "Esta entre el Edificio 97 y 102",
+                "CountryCodePhone": "999",
+                "LandlinePhone": "5555555555",
+                "CellPhone": "5555555555",
+                "ContacteMail": "micorreopersonal@dominio.com",
+                "Latitude": 99999.99,
+                "Longitude": 99999.99,
+                "IsActive": "True"
+            }
+        }
+      }
+    }
+
+
+    res.json ({
+      statusCode: 200,
+      body: responseObject,
+    });
 });
+
+router.post("/login", async function(req, res) {
+  const authHeader = req.headers['authorization']
+
+  if (!authHeader || !authHeader.startsWith('Basic ')) {
+    return res.status(401).send({ message: 'Por favor provee credenciales validos para su autenticacion' });
+  }
+  const base64Credentials = authHeader.split(' ')[1];
+  const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+  const [clientId, clientSecret] = credentials.split(':');
+
+  if (!clientId || !clientSecret) {
+    return res.status(401).send({ message: 'Por favor provee credenciales validos para su autenticacion' });
+  }
+
+  if (clientId != "wjg14gn3zqm34q8srm2htj" || clientSecret != "wjg14gn3zqm34q8srm2htj") {
+    return res.status(401).send({ message: "Credenciales no validas" })
+  }
+  
+  const token = generateToken(clientId, clientSecret)
+
+  return res.status(201).send({token})
+})
 
 app.use('/.netlify/functions/api', router);
 export const handler = serverless(app);
