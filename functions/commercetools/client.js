59d247a41ff3785832c394fcfd614ea2b7d37a9a
Josue <ajpm@thinkcares.com>
2024-10-16 19:03:58 -0600

    Delete functions

--- functions/commercetools/client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiRoot = void 0;
-var buildClient_js_1 = require("./buildClient.js");
-var platform_sdk_1 = require("@commercetools/platform-sdk");
-var credentials_js_1 = require("./credentials.js");
-exports.apiRoot = (0, platform_sdk_1.createApiBuilderFromCtpClient)(buildClient_js_1.ctpClient).withProjectKey({ projectKey: credentials_js_1.CTP_PROJECT_KEY });
--- functions/commercetools/credentials.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CTP_SCOPES = exports.CTP_API_URL = exports.CTP_AUTH_URL = exports.CTP_CLIENT_ID = exports.CTP_CLIENT_SECRET = exports.CTP_PROJECT_KEY = void 0;
-exports.CTP_PROJECT_KEY = "estafeta-dev-001";
-exports.CTP_CLIENT_SECRET = "LQ7WXOLQ7IMxKP15-mS3f46oQCMZc9t3";
-exports.CTP_CLIENT_ID = "zTbSAzn5VslhXJ_BtJ0AFSs2";
-exports.CTP_AUTH_URL = "https://auth.us-central1.gcp.commercetools.com";
-exports.CTP_API_URL = "https://api.us-central1.gcp.commercetools.com";
-exports.CTP_SCOPES = "manage_project:estafeta-dev-001 view_audit_log:estafeta-dev-001";
