0a6d616883c3e15e00a330fe99fe74873aa69095
Josue <ajpm@thinkcares.com>
2024-10-27 18:29:52 -0600

    Prices

--- functions/controllers/quote/index.ts
+++ functions/controllers/quote/index.ts
@@ -15,7 +15,6 @@ router.post("/quote", async(req: Request, res: Response): Promise<any> => {
         service.DeliveryZone = 0
         services.FuelChargeOverweightListPrice = 0
         services.ForwardingLevelCostListPrice = 0
-
         service.TotalAmount = parseFloat((service.ListPrice + service['FuelChargeListPrice ']).toFixed(2));
       }
     } else {
