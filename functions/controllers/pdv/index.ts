50bfd4ec24554acc0d3b76059f0ed1f1942954d5
Josue <ajpm@thinkcares.com>
2024-12-26 18:21:14 -0600

    Completename

--- functions/controllers/pdv/index.ts
+++ functions/controllers/pdv/index.ts
@@ -119,7 +119,7 @@ router.get("/pdv-services", validateToken, async (req: Request, res: Response): 
               "isSender": "0",
               "Alias": destination.alias,
               "TaxPayer": "",
-              "CompleteName": (destination?.firstName ?? "")+" " + (destination?.lastName?? "")+ " " + (destination?.middleName ?? ""),
+              "CompleteName": `${origin?.firstName ?? ""} ${origin?.lastName ?? ""} ${origin?.middleName ?? ""}`,
               "zipCode": destination.postalCode,
               "roadTypeCode": "",
               "roadTypeName": destination.road,
