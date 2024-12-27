7f8ee3a2f006a7c3d30389db195bf154fc57aa71
Josue <ajpm@thinkcares.com>
2024-12-27 15:25:21 -0600

    Normalizar

--- functions/validateDate/validate.js
+++ functions/validateDate/validate.js
@@ -4,8 +4,8 @@ exports.checkDate = checkDate;
 function checkDate(date, endDate) {
     const dateOrder = new Date(date);
     const dateNow = endDate ? new Date(endDate) : new Date();
-    dateOrder.setHours(0, 0, 0, 0);
-    dateNow.setHours(0, 0, 0, 0);
+    dateOrder.setUTCHours(0, 0, 0, 0);
+    dateNow.setUTCHours(0, 0, 0, 0);
     console.log('Fecha enviada (normalizada):', dateOrder);
     console.log('Fecha actual (normalizada):', dateNow);
     const dayDiff = dateNow.getTime() - dateOrder.getTime();
--- functions/validateDate/validate.ts
+++ functions/validateDate/validate.ts
@@ -2,9 +2,9 @@ export function checkDate(date: string, endDate?: string) {
   const dateOrder = new Date(date);
   const dateNow = endDate ? new Date(endDate) : new Date();
 
-  dateOrder.setHours(0, 0, 0, 0);
+  dateOrder.setUTCHours(0, 0, 0, 0);
 
-  dateNow.setHours(0, 0, 0, 0);
+  dateNow.setUTCHours(0, 0, 0, 0);
 
   console.log('Fecha enviada (normalizada):', dateOrder);
   console.log('Fecha actual (normalizada):', dateNow);
