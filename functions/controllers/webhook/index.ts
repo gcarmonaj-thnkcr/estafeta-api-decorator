e47e28056ed2db7f0d7ce0685376831ceaed08c7
Josue <ajpm@thinkcares.com>
2025-01-17 09:46:16 -0600

    ENvs

--- functions/controllers/webhook/index.ts
+++ functions/controllers/webhook/index.ts
@@ -8,8 +8,6 @@ const router = Router()
 router.post("/payment/webhook", async (req: Request, res: Response): Promise<any> => {
   try{
     console.log(req.body)
-    console.log(req)
-    return res.sendStatus(200)
     const paymentInfo: ITransactionEvent = req.body
     console.log(paymentInfo.transaction)
     if(paymentInfo.transaction.status != "completed") return res.sendStatus(200)
