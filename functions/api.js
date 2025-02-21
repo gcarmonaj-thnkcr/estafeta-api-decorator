3598d4bf5344f47e80a293d8457775552ea36981
Josue <ajpm@thinkcares.com>
2025-02-21 13:06:13 -0600

    Console

--- functions/api.js
+++ functions/api.js
@@ -30,6 +30,7 @@ exports.handler = void 0;
 const express_1 = __importDefault(require("express"));
 const serverless_http_1 = __importDefault(require("serverless-http"));
 const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
 const dotenv = __importStar(require("dotenv"));
 // import data from './mock_values.json' assert { type: 'json'}
 const index_1 = __importDefault(require("./controllers/pdv/index"));
@@ -43,6 +44,7 @@ const index_8 = __importDefault(require("./controllers/reprocess/index"));
 const app = (0, express_1.default)();
 dotenv.config();
 app.use((0, cors_1.default)());
+app.use((0, morgan_1.default)('combined'));
 app.use(express_1.default.json());
 app.use("/.netlify/functions/api", index_6.default);
 app.use("/.netlify/functions/api", index_1.default);
